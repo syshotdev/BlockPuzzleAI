062536f7b372819b962f7c183ec6a446ae115b20
Syshotdev <syshotdev@gmail.com>
2024-02-11 14:22:11 -0800

    Added rotating, blocks, blocks on board, and many other things. Ready for AI.

--- Objects/Block/Block.gd
+++ Objects/Block/Block.gd
@@ -2,32 +2,73 @@ extends Node2D
 
 class_name Block
 
-const shape_1x1 = [1]
-const shape_2x2 = [
+const shape_1x1 := [
+	[1]]
+const shape_2x2 := [
 	[1,1],
 	[1,1]]
-const shape_3x3 = [
+const shape_3x3 := [
 	[1,1,1],
 	[1,1,1],
 	[1,1,1]]
-const shape_L = [
+const shape_L := [
 	[1,0],
 	[1,0],
 	[1,1]]
-const shape_perfectL = [
+const shape_perfectL := [
 	[1,0,0],
 	[1,0,0],
 	[1,1,1]] # Shape is L but both sides are equal
-const shape_T = [
+const shape_T := [
 	[1,1,1],
 	[0,1,0]]
-const shape_Ix3 = [
+const shape_Ix3 := [
 	[1,1,1]]
-const shape_Ix4 = [
+const shape_Ix4 := [
 	[1,1,1,1]]
-const shape_Ix5 = [
+const shape_Ix5 := [
 	[1,1,1,1,1]]
-const shape_Z = [
+const shape_Z := [
 	[0,1],
 	[1,1],
-	[1,0]] # Will flip later
+	[1,0]]
+const shape_S := [
+	[1,0],
+	[1,1],
+	[0,1]]
+
+
+const blockTypes = [
+	shape_1x1,
+	shape_2x2,
+	shape_3x3,
+	shape_perfectL,
+	shape_L,
+	shape_T,
+	shape_Z,
+	shape_S,
+	shape_Ix3,
+	shape_Ix4,
+	shape_Ix5]
+
+
+var rotatedBlocks : Dictionary # Key: Const block, Value: Array of all rotated blocks (NO DUPLICATES)
+
+
+func _init():
+	calculateRotatedBlocks()
+
+
+func calculateRotatedBlocks():
+	for block0 in blockTypes:
+		# Array of blocks to be put into rotatedBlocks
+		var blockRotateds : Array
+		# Vars = block + degrees rotated
+		var block90 := MatrixOperations.rotateMatrixClockwise(block0)
+		var block180 := MatrixOperations.rotateMatrixClockwise(block90)
+		var block270 := MatrixOperations.rotateMatrixClockwise(block180)
+		
+		# Was going to do some optimizations where arrays check each other to see if there are duplicates,
+		# But duplicates aren't gonna kill the game so whatever.
+		blockRotateds.append_array([block0, block90, block180, block270])
+		rotatedBlocks[block0] = blockRotateds
--- Objects/Useful Scripts/MatrixOperations.gd
+++ Objects/Useful Scripts/MatrixOperations.gd
@@ -2,6 +2,13 @@ extends Node
 
 class_name MatrixOperations
 
+enum numDegrees{
+	ZERO,
+	NINETY,
+	ONE_HUNDRED_EIGHTY,
+	TWO_HUNDRED_SEVENTY
+}
+
 # Copied basically from https://github.com/jpcerrone/PokeTetris
 static func createMatrix(sizeX : int, sizeY : int) -> Array[Array]:
 	var matrix : Array[Array]
@@ -10,9 +17,22 @@ static func createMatrix(sizeX : int, sizeY : int) -> Array[Array]:
 	for y in range(sizeY):
 		var row : Array = []
 		row.resize(sizeX)
+		row.fill(0)
 		matrix.append(row)
 	
 	return matrix
+
+# Rotates clockwise only because math complicated so lazy
+static func rotateMatrixClockwise(matrix : Array) -> Array[Array]:
+	var size : Vector2i = Vector2(matrix[0].size(), matrix.size())
+	var newMatrix := MatrixOperations.createMatrix(size.y, size.x)
+	
+	for x in range(size.y):
+		for y in range(size.x):
+			# size.y - x - 1 because backwards for some reason (-1 = not out of bounds)
+			newMatrix[y][x] = matrix[size.y - x - 1][y]
+	
+	return newMatrix
 
 
 static func clearRow(matrix : Array[Array], row : int):
